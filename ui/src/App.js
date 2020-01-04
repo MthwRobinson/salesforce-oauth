ce18bbb0c9264228687409e37c5dc73f4c0ed1e4
matt <mthw.wm.robinson@gmail.com>
2020-01-04 15:47:18 -0500

    wrote authentication instructions

--- ui/src/App.js
+++ ui/src/App.js
@@ -77,15 +77,16 @@ class App extends Component {
 
         <div className="app-body">
           <Row>
-            <Col xs={6} sm={6} md={6} lg={6}>
-            <h4>Let's Connect to SalesForce!</h4>
-              <p>This page provides instructions for connecting to the SalesForce API using the OAUTH2 workflow. OAUTH2 enables an app to integrate with SalesForce on behalf of the user without having to store the user's SalesForce credentials.</p>
-              <h4>Register a Connect App on SalesForce</h4>
-              <h4>Authenticate with SalesForce</h4>
-              <p>Click <a href={authorizeURL}>this link</a> to authenticate with SalesForce. After you authenticate with SalesForce, you will be redirected back to this page. When SalesForce redirects you, it will include an access code that you can use to interact with SalesForce via the API. You'll see the access code populate below.</p>
+            <Col xs={8} sm={8} md={8} lg={8}>
+              <h4>Let's Connect to SalesForce!</h4>
+              <p>This page provides instructions for connecting to the SalesForce API using the OAuth2 workflow. OAuth2 enables an app to integrate with SalesForce on behalf of the user without having to store the user's SalesForce credentials.</p>
+              <h4>Step 1: Register a Connected App on SalesForce</h4>
+              <p>The first step is to register a Connected App on your Salesforce account. To register a Connected App, go to your Setup screen, click "App Manager", and then click "New Connected App" in the top right corner. From there, fill out the basic information for your app, and then check the "Enable OAuth Settings". Once that's done, enter <b>{REDIRECT_URL}</b> as the callback URL and move "Full access" over to the "Selected OAuth Scopes".</p>
+              <h4>Step 2: Authenticate with SalesForce</h4>
+              <p>Use the form to the right to authenticate with Salesforce. Enter your Consumer Key from the Connected App into the first field, and then use the drop down to indicate whether you are authenticating to a production instance or a sandbox instance. Production instances authenticate with <b>https://login.salesforce.com</b>, whereas sandbox instances authenticate with <b>https://test.salesforce.com</b>. When you're ready, hit "Authenticate". The first time you do this, you'll be redirected to a Salesforce login page, which will ask you to authorize the Connected App to make API requests on behalf of your account. Once you've logged in, you'll be redirected back to this page, and an access code will appear below. If you've already authorized the Salesforce integration, this page will simply refresh with an access code.</p>
             {accessCodeMessage}
             </Col>
-            <Col xs={6} sm={6} md={6} lg={6}>
+            <Col xs={4} sm={4} md={4} lg={4}>
               <h4>Obtain an Access Code</h4>
               <Form onSubmit={this.handleSubmit} >
                 <FormGroup className="pullLeft">
@@ -112,7 +113,7 @@ class App extends Component {
                   className="login-button pullRight"
                   bsStyle="primary"
                   href={authorizeURL}
-                >Submit</Button>
+                >Authenticate</Button>
               </Form>
             </Col>
           </Row>
